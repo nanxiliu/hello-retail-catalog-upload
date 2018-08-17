9f137ed995b51743bd035c5a30b2b0aac6ee7424
Liu <nanxi@mit.edu>
2018-08-17 12:01:10 -0700

    Skip empty lines and ensure there's no leading or trailing spaces when sending a styleId to SQS

--- hello-retail-send-styleIds.py
+++ hello-retail-send-styleIds.py
@@ -4,9 +4,19 @@ import boto3
 SQS_CLIENT = boto3.client('sqs')
 
 def lambda_handler(event, context):
-    return(SQS_CLIENT.send_message(
-        QueueUrl='',
-        MessageBody='4912233'
-    ))
+    # Replace the txt file with your appropriate file with styleIds
+    with open("styleIds.txt", "r") as file:
+        for line in file:
+            # Skip the line if it's empty
+            if line.strip() == "":
+                continue
+            # Ensure there's no leading or trailing spaces
+            styleId = str(line).strip()
+            # Replace with correct queue url
+            SQS_CLIENT.send_message(
+                QueueUrl='',
+                MessageBody= styleId
+            )
+    return ''
 
 
